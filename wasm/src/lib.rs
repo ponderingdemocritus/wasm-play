4efaab32fb0254c15c21f2ada16f98c89ea0b7f6
Gregory Edison <gregory.edison1993@gmail.com>
2023-01-17 18:08:07 +0100

    feat: wasm

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -11,8 +11,8 @@ use cairo_rs::{
     vm::{runners::cairo_runner::CairoRunner, vm_core::VirtualMachine},
 };
 use num_bigint::BigInt;
-use std::path::Path;
-use std::{collections::HashMap, io::Cursor};
+use serde::{Deserialize, Serialize};
+use std::io::Cursor;
 use wasm_bindgen::prelude::*;
 
 macro_rules! bigint {
@@ -85,38 +85,42 @@ pub struct CairoOutput {
     pub y_positions: Vec<BigInt>,
 }
 
-fn print_two_array_hint(
-    vm: &mut VirtualMachine,
-    _exec_scopes: &mut ExecutionScopes,
-    ids_data: &HashMap<String, HintReference>,
-    ap_tracking: &ApTracking,
-    _constants: &HashMap<String, BigInt>,
-) -> Result<(), VirtualMachineError> {
-    let x_len = get_integer_from_var_name("x_fp_s_len", vm, ids_data, ap_tracking)?
-        .to_u32_digits()
-        .1[0];
-    let y_len = get_integer_from_var_name("y_fp_s_len", vm, ids_data, ap_tracking)?
-        .to_u32_digits()
-        .1[0];
-    let x = get_ptr_from_var_name("x_fp_s", vm, ids_data, ap_tracking)?;
-    let y = get_ptr_from_var_name("y_fp_s", vm, ids_data, ap_tracking)?;
-    for i in 0..x_len as usize {
-        let word_address = Relocatable {
-            segment_index: x.segment_index,
-            offset: i,
-        };
-        let value = vm.get_integer(&word_address)?;
-        console_log!("{}", value);
+#[wasm_bindgen(js_name = runCairoProgram)]
+pub fn run_cairo_program_wasm(
+    num_pts: u32,
+    theta_0_deg: i32,
+    v_0: u32,
+) -> Result<JsValue, JsError> {
+    let vm = run_cairo_program(num_pts, theta_0_deg, v_0).unwrap();
+    let mut x_positions: Vec<BigInt> = vec![];
+    let mut y_positions: Vec<BigInt> = vec![];
+    if let [x_len_big, maybe_x, y_len_big, maybe_y] = &vm.get_return_values(4).unwrap()[..] {
+        let x_len = x_len_big.get_int_ref().unwrap().to_u32_digits().1[0];
+        let y_len = y_len_big.get_int_ref().unwrap().to_u32_digits().1[0];
+        let x = maybe_x.get_relocatable().unwrap();
+        let y = maybe_y.get_relocatable().unwrap();
+        for i in 0..x_len {
+            let word_address = Relocatable {
+                segment_index: x.segment_index,
+                offset: i as usize,
+            };
+            x_positions.push(vm.get_integer(&word_address).unwrap().into_owned());
+        }
+        for i in 0..y_len {
+            let word_address = Relocatable {
+                segment_index: y.segment_index,
+                offset: i as usize,
+            };
+            y_positions.push(vm.get_integer(&word_address).unwrap().into_owned());
+        }
     }
-    for i in 0..y_len as usize {
-        let word_address = Relocatable {
-            segment_index: y.segment_index,
-            offset: i,
-        };
-        let value = vm.get_integer(&word_address)?;
-        console_log!("{}", value);
-    }
-    Ok(())
+    Result::Ok(
+        serde_wasm_bindgen::to_value(&CairoOutput {
+            x_positions: x_positions,
+            y_positions: y_positions,
+        })
+        .unwrap(),
+    )
 }
 
 #[cfg(test)]
